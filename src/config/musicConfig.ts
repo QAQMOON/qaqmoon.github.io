dcffbd0b5ce340210123d1c18df7106573628ce5
moon <3012546069@qq.com>
2026-06-25 00:50:15 +0800

    Show music player control

--- src/config/musicConfig.ts
+++ src/config/musicConfig.ts
@@ -2,7 +2,7 @@ import type { MusicPlayerConfig } from "../types/config";
 
 export const musicPlayerConfig: MusicPlayerConfig = {
 	enable: true,
-	showFloatingPlayer: false,
+	showFloatingPlayer: true,
 	floatingEntryMode: "fab",
 	mode: "local",
 	meting_api: "",
